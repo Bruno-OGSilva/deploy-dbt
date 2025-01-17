9dda01383ac372fdf57e803b27f44576d8ff556c
Bruno Silva <bruno.ogs@gmail.com>
2025-01-16 22:32:42 -0500

    update

---
+++ northwind/models/staging/stg_customers.sql
@@ -0,0 +1,24 @@
+-- models/staging/stg_customers.sql
+wtih sources as(
+    select * from {{ ref('raw_customers')}}
+),
+
+
+with renamed_and_cleaning as(
+select
+    customer_id,
+    company_name,
+    contact_name,
+    contact_title,
+    address,
+    city,
+    region,
+    postal_code,
+    country,
+    phone,
+    fax
+from
+    sources
+)
+
+select * from renamed_and_cleaning
